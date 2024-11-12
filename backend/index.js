b3c1860a40be3a5b7df4c65bf4e5198f9aaa1dfd
yaouba <kabayyaouba@gmail.com>
2024-11-12 10:51:48 +0100

    Add database onnectin and env file

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,17 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-
-
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
-
-
 import mergedResolvers from "./resolvers/index.js";
 import mergedRypeDefs from "./typeDefs/index.js";
+import { connectDB } from "./db/connectDB.js";
 
+dotenv.config();
 const app = express();
 
 const httpServer = http.createServer(app);
@@ -37,5 +35,7 @@ app.use(
 )
 
 await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+
+await connectDB();
 
 console.log(`🚀 Server ready at http://localhost:4000`);
